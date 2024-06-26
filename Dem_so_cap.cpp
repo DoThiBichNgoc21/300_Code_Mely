b101d837347f2c1f9d0e3c9bd24221cc541e770a
DoThiBichNgoc21 <161515353+DoThiBichNgoc21@users.noreply.github.com>
2024-06-26 17:42:55 +0700

    Create Dem_so_cap.cpp

---
+++ Dem_so_cap.cpp
@@ -0,0 +1,27 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    long long N, X;
+    cin >> N >> X;
+    long long a[N];
+    for ( int i = 0; i < N; i++){
+        cin >> a[i];
+    }
+    int cnt = 0;
+    for ( int i = 0; i< N; i++ ){
+        for ( int j = i; j < N; j++ ){
+            if ( a[i] * a[i] + a[j] == X ){
+                cnt++;
+            }
+        }
+    }
+    cout << cnt;
+    return 0;
+}
+
