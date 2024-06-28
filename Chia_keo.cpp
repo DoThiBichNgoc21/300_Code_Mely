1e81edbb83dc9b19ffc0afa6681e32082b8c7b73
DoThiBichNgoc21 <161515353+DoThiBichNgoc21@users.noreply.github.com>
2024-06-28 23:55:52 +0700

    sub

--- Chia_keo.cpp
+++ Chia_keo.cpp
@@ -1,1 +1,23 @@
+#include <bits/stdc++.h>
 
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while ( t != 0 ){
+        int n, m;
+        cin >> n >> m;
+        long long a[n];
+        for ( int i = 0; i < n; i++ ){
+            cin >> a[i];
+        }
+        long long sum = 0;
+        for ( int i = 0; i < n; i++ ){
+             sum += ( (m / ( a[i] + 1 ) + ( m % ( a[i] + 1 ))));
+        }
+        cout << sum << endl;
+        t--;
+    }
+    return 0;
+}
--- Xep_hinh_vuong.cpp
+++ Xep_hinh_vuong.cpp
@@ -1,1 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while( t != 0 ){
+        int n;
+        cin >> n;
+        long long a = sqrt(n);
+        while ( n % a != 0 ){
+            a--;
+        }
+        cout << a << " " << n / a << endl;
+        t--;
+    }
+    return 0;
+}
 
